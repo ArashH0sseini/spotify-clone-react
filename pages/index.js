538af7810826ddc88fb1b988a540eed7be94cdc2
Arash Hosseini <arashhosseini733@gmail.com>
2022-08-22 17:15:53 +0430

    Building the Sidebar Component

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import Sidebar from "../components/Sidebar";
 
 export default function Home() {
   return (
     <div>
-      <main>
-        <div>salam</div>
+      <main className="bg-black h-screen overflow-hidden">
+        <Sidebar />
       </main>
+      <div>
+        
+      </div>
     </div>
   )
 }
