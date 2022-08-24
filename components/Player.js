a5273ae4ea20861fff9b601052ffc96528af8261
Arash Hosseini <arashhosseini733@gmail.com>
2022-08-24 18:43:56 +0430

    Building the Player Component

---
+++ components/Player.js
@@ -0,0 +1,26 @@
+/* eslint-disable @next/next/no-img-element */
+import { useSession } from 'next-auth/react'
+import React,{useState} from 'react'
+import { useRecoilState } from 'recoil'
+import { currentTrackIdState, isPlayingState } from '../atoms/songAtom'
+import useSpotify from '../hooks/useSpotify'
+
+function Player() {
+    const spotifyApi = useSpotify()
+    const { data: session, status } = useSession()
+    const [currentTrackId, setCurrentTrackIdState] =
+        useRecoilState(currentTrackIdState);
+    const [isPlaying, setIsPlaying] = useRecoilState(isPlayingState)
+    const [volume, setVolume] = useState(50)
+
+
+    return (
+        <div>
+            <div>
+                <img src='' alt='' />
+            </div>
+        </div>
+    )
+}
+
+export default Player
